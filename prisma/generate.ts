474da3b77ebda77093cf63314da48ca719fbb73f
jennydunix <jennifer.o.odunze@gmail.com>
2023-07-20 15:34:47 +0100

    added index.ts file

--- prisma/generate.ts
+++
@@ -1,9 +0,0 @@
-import { execSync } from 'child_process';
-
-try {
-  execSync('npx prisma generate', { stdio: 'inherit' });
-  console.log('Prisma Client generated successfully!');
-} catch (error) {
-  console.error('Error generating Prisma Client:', error);
-  process.exit(1);
-}
